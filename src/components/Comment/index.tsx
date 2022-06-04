be46c2df3780860cdea4a7372a823955990c9a4a
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-06-04 17:13:44 -0300

    chore: create Comment component

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './styles.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/ViniciusHack.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vinicius Hack</strong>
+              <time title="4 de junho às 15:20h" dateTime="2022-06-04 15:20:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,4 +1,5 @@
 
+import { Comment } from '../Comment'
 import styles from './styles.module.css'
 
 export function Post() {
@@ -38,6 +39,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
