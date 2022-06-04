21a657ab0beb9eb371f3ad74ada2e6ea887256b3
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-06-04 19:41:11 -0300

    feat: likes

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,4 +1,5 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from '../Avatar';
 import styles from './styles.module.css';
 
@@ -8,9 +9,14 @@ interface CommentProps {
 }
 
 export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
 
   const handleDeleteComment = () => {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((prevState) => prevState + 1)
   }
 
   return (
@@ -34,9 +40,9 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
