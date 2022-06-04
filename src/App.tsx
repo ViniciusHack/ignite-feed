f8a04d209ca88b962438d0087bd8d2d54cb79b1b
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-06-04 16:15:48 -0300

    chore: create Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Header } from "./components/Header"
-import './global.css'
-
+import styles from './App.module.css';
+import { Header } from "./components/Header";
+import { Sidebar } from './components/Sidebar';
+import './global.css';
 
 export function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </div>
   )
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,29 @@
+
+import { PencilLine } from 'phosphor-react';
+import styles from './styles.module.css';
+
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+
+        <img className={styles.avatar} src="https://github.com/ViniciusHack.png" />
+
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+
+        <footer>
+          <a href="#">
+            <PencilLine size={20}/>Editar seu perfil
+          </a>
+        </footer>
+      </div>
+    </aside>
+  );
+}
